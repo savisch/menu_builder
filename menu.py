6a96cb7f60bbee0a41bfd5af2a01f532d30b0627
savisch <savisch@gmail.com>
2021-01-19 18:37:42 -0600

    first commit

---
+++ menu.py
@@ -0,0 +1,17 @@
+import random
+
+breakfast = ["eggs", "bacon", "oats"]
+lunch = ["burrito", "quesadilla", "burger"]
+dinner = ["carnitas", "fish", "meatballs"]
+
+def choose_item (lst):
+    choice = random.choice(lst)
+    lst.pop(lst.index(choice))
+    return choice
+
+for i in range(3):
+    print("Day" + str(i + 1))
+    print("Breakfast: " + choose_item(breakfast))
+    print("Lunch: " + choose_item(lunch))
+    print("Dinner: " + choose_item(dinner))
+    print()
